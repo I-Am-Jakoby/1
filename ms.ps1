6bba01cdef741dc408ea769848c81117666531fb
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2022-11-02 22:19:03 -0500

    Update ms.ps1

--- ms.ps1
+++ ms.ps1
@@ -2,6 +2,7 @@ $channels = @"
 https://www.youtube.com/iamjakoby
 https://www.youtube.com/c/CosmodiumCS
 https://www.youtube.com/c/zSecurity
+https://www.youtube.com/c/STOKfredrik
 https://www.youtube.com/c/SystemExploited/featured
 https://www.youtube.com/c/Lab401
 https://www.youtube.com/c/TheCyberMentor
